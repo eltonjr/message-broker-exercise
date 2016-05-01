8821e52075a888f47039ef6f7f94b19526e7a949
Elton Hoffmann <elton.hoffmann@neoway.com.br>
2016-04-30 23:18:48 -0300

    Connecting two services with rabbit

--- aggregator/index.js
+++ aggregator/index.js
@@ -8,18 +8,21 @@ connection.on('ready', function () {
 	
 	console.log('Wooooow... we are readyyy');
 
+	var exchange = connection.exchange(rabbitConfig.exchange, {});
+
 	// Use the default 'amq.topic' exchange
 	connection.queue(rabbitConfig.queue, function (q) {
 
 		console.log('Conected to queue');
 
 		// Catch all messages
-		q.bind(rabbitConfig.route);
+		q.bind(exchange, rabbitConfig.route);
 
 		// Receive messages
-		q.subscribe(function (message) {
+		q.subscribe(function (message, headers, deliveryInfo, messageObject) {
 			// Print messages to stdout
-			console.log(message);
+			console.log('Got a message with route ' + deliveryInfo.routingKey + ' and data:');
+			console.log(JSON(message.data));
 		});
 	});
 });
--- online-store/config/rabbit.js
+++ online-store/config/rabbit.js
@@ -1,6 +1,5 @@
 module.exports = {
 	host: 'localhost',
 	exchange: 'amqp.topic',
-	queue: 'store.orders',
-	route: '#'
+	route: 'orders.online'
 };
